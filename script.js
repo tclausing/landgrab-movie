8713bc5fee533f7d4518fe170f3f8b2577d990a1
tclausing <clausing3@gmail.com>
2017-04-21 02:12:33 -0500

    initial

---
+++ script.js
@@ -0,0 +1,144 @@
+(function() {
+
+  var storage = chrome.storage.local;
+  storage.get('uris', function(items) {
+    captureScreenshots(items.uris || []);
+  });
+
+  function captureScreenshots(uris) {
+    var el = document.getElementById('imgdiv');
+
+    // domtoimage.toJpeg(el, {
+    //     quality: 0.95
+    //   })
+    domtoimage.toPixelData(el)
+      .then(function(pixels) {
+
+        var width = el.offsetWidth, height = el.offsetHeight;
+
+        var encoder = new WebPEncoder();
+        // http://libwebpjs.appspot.com/v0.1.3/
+        encoder.WebPEncodeConfig({
+          method: 3
+        });
+        var out = {};
+        encoder.WebPEncodeRGBA(pixels, width, height, width*4, 80, out);
+        var b64 = btoa(out.output);
+        uris.push('data:image/webp;base64,' + b64);
+
+        var btn = document.querySelector('#forward_one_btn');
+        if (btn.getAttribute('btn_disabled') === 'true') {
+          // debugger;
+          storage.clear();
+          createVideo(uris, width, height);
+        } else {
+          storage.set({
+            uris: uris
+          }, function() {
+            btn.click();
+          });
+        }
+      });
+  }
+
+
+  function createVideo(uris, width, height) {
+    /* adapted from http://techslides.com/demos/image-video/create.html */
+
+    var canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    var context = canvas.getContext("2d");
+    // https://github.com/antimatter15/whammy
+    var video = new Whammy.Video(5);
+
+    function next() {
+      if (uris.length) {
+        // process(uris.shift(), canvas, context, video, next);
+        // debugger;
+        video.add(uris.shift());
+        next();
+      } else {
+        finalizeVideo(video);
+      }
+    }
+
+    next();
+  }
+
+  function process(dataUri, canvas, context, video, next) {
+
+    var img = new Image();
+
+    //load image and drop into canvas
+    img.onload = function() {
+
+      //a custom fade in and out slideshow
+      context.globalAlpha = 0.2;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      video.add(context);
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      context.globalAlpha = 0.4;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      video.add(context);
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      context.globalAlpha = 0.6;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      video.add(context);
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      context.globalAlpha = 0.8;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      video.add(context);
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      context.globalAlpha = 1;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+      //this should be a loop based on some user input
+      video.add(context);
+      video.add(context);
+      video.add(context);
+      video.add(context);
+      video.add(context);
+      video.add(context);
+      video.add(context);
+
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      context.globalAlpha = 0.8;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      video.add(context);
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      context.globalAlpha = 0.6;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      video.add(context);
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      context.globalAlpha = 0.4;
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      video.add(context);
+
+      next();
+    };
+
+    img.src = dataUri;
+  }
+
+  function finalizeVideo(video) {
+    // var start_time = +new Date;
+    video.compile(false, function(output) {
+
+      // var end_time = +new Date;
+      var url = URL.createObjectURL(output);
+
+      var link = document.createElement('a');
+      link.download = 'landgrab.webm';
+      link.href = url;
+      link.click();
+// debugger;
+      var div = document.createElement('div');
+      div.innerHTML = '<video controls autoplay loop></video>';
+      var videoEl = div.childNodes[0];
+      document.body.insertBefore(videoEl, document.body.firstChild);
+      videoEl.src = url;
+      // document.getElementById('status').innerHTML = "Compiled Video in " + (end_time - start_time) + "ms, file size: " + Math.ceil(output.size / 1024) + "KB";
+    });
+  }
+})();
